b8ca54b7126e407e850cd44a774592ad53096c54
lamndl <lva26195@gmail.com>
2022-03-19 01:00:24 +0700

    vscode commit test

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-g++ -fdiagnostics-color=always -g src\\*.cpp -o build/game.exe -Iinclude -Llib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
+g++ -fdiagnostics-color=always -g -Wall src\\*.cpp -o build/game.exe -Iinclude -Llib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
 cd build
 game
