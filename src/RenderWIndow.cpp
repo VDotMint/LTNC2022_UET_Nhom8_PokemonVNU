b2c9454a2d194d0388343306e5daba52e1b4d901
VDotMint <uts.lvm.technology@gmail.com>
2022-03-19 10:13:54 +0700

    changed window size and scaling

--- src/RenderWIndow.cpp
+++ src/RenderWIndow.cpp
@@ -1,5 +1,8 @@
 #include "RenderWindow.h"
 
+const int SCREEN_WIDTH = 832;
+const int SCREEN_HEIGHT = 704;
+
 RenderWindow::RenderWindow() {
     window=nullptr;
 	renderer=nullptr;
@@ -12,7 +15,7 @@ void RenderWindow::initSystem(){
  	if (!(IMG_Init(IMG_INIT_PNG))) {
  		std::cerr<<"IMG_Init Error"<<SDL_GetError()<<std::endl;
 	}
-    window=SDL_CreateWindow("VNUmon",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480,0);
+    window=SDL_CreateWindow("Pokemon VNU", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 	if (window==nullptr) std::cerr<<"CreateWindow Error";
 	renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
 	if (renderer==nullptr) std::cerr<<"CreateRenderer Error";
@@ -20,7 +23,7 @@ void RenderWindow::initSystem(){
 };
 
 void RenderWindow::render(const char* path) {
-	SDL_Rect dst={0,0,64*4,64*4};
+	SDL_Rect dst={0,0,SCREEN_WIDTH,SCREEN_HEIGHT};
 	SDL_Texture* texture=loadTexture(path);
 	SDL_RenderCopy(renderer,texture,NULL,&dst);
 	SDL_RenderPresent(renderer);
