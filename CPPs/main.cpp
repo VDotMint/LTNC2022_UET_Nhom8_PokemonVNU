d51cff85ef80dd78f5d9a81100686df676dff1a1
VDotMint <uts.lvm.technology@gmail.com>
2022-04-15 17:04:22 +0700

    camera moves smoother

--- CPPs/main.cpp
+++ CPPs/main.cpp
@@ -12,6 +12,8 @@
 
 using namespace std;
 
+const int MOVE_SPEED = 4;
+
 RenderWindow renderWindow;
 TileSheet g2TileSheet;
 Map g2Map;
@@ -22,6 +24,8 @@ SDL_Rect camera = {0, 0, 64*13, 64*11};
 bool quit = false;
 void initSystem();
 void gameLoop();
+void moveCamera();
+void finishMovement();
 
 int main(int argc, char* argv[]) {
     initSystem();
@@ -46,6 +50,62 @@ void initSystem() {
     }
 }
 
+bool isMoving = false;
+bool finishMove = false;
+bool moveUp = false, moveLeft = false, moveDown = false, moveRight = false;
+
+void moveCamera() {
+    if (moveRight == true) {
+        isMoving = true; 
+        camera.x += MOVE_SPEED;
+    } else if (moveLeft == true) {
+        isMoving = true; 
+        camera.x -= MOVE_SPEED;
+    } else if (moveUp == true) {
+        isMoving = true; 
+        camera.y -= MOVE_SPEED;
+    } else if (moveDown == true) {
+        isMoving = true; 
+        camera.y += MOVE_SPEED;
+    }
+}
+
+void finishMovement() {
+    if (moveRight == true) {
+        if (camera.x % 64 != 0) {
+            camera.x += MOVE_SPEED;
+        } else {
+            moveRight = false;
+            isMoving = false;
+            finishMove = false;
+        }
+    } else if (moveLeft == true) {
+        if (camera.x % 64 != 0) {
+            camera.x -= MOVE_SPEED;
+        } else {
+            isMoving = false;
+            moveLeft = false;
+            finishMove = false;
+        }
+    } else if (moveUp == true) {
+        if (camera.y % 64 != 0) {
+            camera.y -= MOVE_SPEED;
+        } else {
+            isMoving = false;
+            moveUp = false;
+            finishMove = false;
+        }
+    } else if (moveDown == true) {
+        if (camera.y % 64 != 0) {
+            camera.y += MOVE_SPEED;
+        } else {
+            isMoving = false;
+            moveDown = false;
+            finishMove = false;
+        }
+    }
+}
+
 void gameLoop() {
     SDL_Event e;
 
@@ -56,25 +116,47 @@ void gameLoop() {
                 quit = true;
             } else if (e.type == SDL_MOUSEBUTTONDOWN) {
                 cerr << e.motion.x << " " << e.motion.y << endl;
-            } else if (e.type == SDL_KEYDOWN) {
+            } else if (e.type == SDL_KEYDOWN and isMoving == false and e.key.repeat == 0) {
                 switch (e.key.keysym.sym) {
                     case SDLK_w:
-                        camera.y -= 64;                    
-                        break;
+                        cout << "Key pressed: W" << endl;
+                        moveUp = true; break;
                     case SDLK_a:
-                        camera.x -= 64;                    
-                        break;
+                        cout << "Key pressed: A" << endl;
+                        moveLeft = true; break;
                     case SDLK_s:
-                        camera.y += 64;
-                        break;
+                        cout << "Key pressed: S" << endl;
+                        moveDown = true; break;
                     case SDLK_d:
-                        camera.x += 64;
-                        break;
+                        cout << "Key pressed: D" << endl;
+                        moveRight = true; break;
                     default:
                         break;
                 }
-                mainPlayer.setPlayerCoords(camera.x/64, camera.y/64);
+            } else if (e.type == SDL_KEYUP and isMoving == true and e.key.repeat == 0) {
+                switch (e.key.keysym.sym) {
+                    case SDLK_w:
+                        cout << "Key released: W" << endl;
+                        finishMove = true; break;
+                    case SDLK_a:
+                        cout << "Key released: A" << endl;
+                        finishMove = true; break;
+                    case SDLK_s:
+                        cout << "Key released: S" << endl;
+                        finishMove = true; break;
+                    case SDLK_d:
+                        cout << "Key released: D" << endl;
+                        finishMove = true; break;
+                    default:
+                        break;
+                }
             }
+        }
+
+        if (finishMove == true) {
+            finishMovement();
+        } else {
+            moveCamera();
         }
 
         if (camera.x > g2Map.getMapWidth()*64 - 832) {
@@ -86,6 +168,8 @@ void gameLoop() {
         } else if (camera.y < 0) {
             camera.y += 64;
         }
+
+        mainPlayer.setPlayerCoords(camera.x/64, camera.y/64);
 
         SDL_Delay(1000/60);
 
