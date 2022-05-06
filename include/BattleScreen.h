be03ef3040f05d7138f2d3e7ba24d53bdb4eb71e
VDotMint <uts.lvm.technology@gmail.com>
2022-05-06 18:17:24 +0700

    opps forgot to stage a change

--- include/BattleScreen.h
+++ include/BattleScreen.h
@@ -3,21 +3,26 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "RenderWindow.h"
+#include "mPlayer.h"
 
+class BattleScreenButtons {
+	private:
 
-static double pCirX = 0;
-static double oCirX = 0;
+	public:
+
+};
 
 class BattleScreen {
 	private:
 		SDL_Texture* battleBackground;
 		SDL_Texture* battleCircle;
-		SDL_Rect playerCircle;
-		SDL_Rect opponentCircle;
+		SDL_Texture* grayInputBox;
+		SDL_Rect playerCircle, opponentCircle, grayBoxRect;
 	public:
+		bool scrollingPlayer;
 		BattleScreen();
 		~BattleScreen();
-		void initBattleScreen();
+		void initBattleScreen(mPlayer* player, Trainer* opponent);
 		void freeBattleScreen();
 		void drawBattleScreen(bool fMtB, bool fBtM);
 };
